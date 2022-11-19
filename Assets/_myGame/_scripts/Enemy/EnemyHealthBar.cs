292a6248d25b445891c919bf40ebb0813e3d3201
Azizhan Çil <azo.DevV@gmail.com>
2022-11-20 02:17:36 +0300

    EnemyHealthBar

---
+++ Assets/_myGame/_scripts/Enemy/EnemyHealthBar.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    [SerializeField] Image _healthBarSprite;
+    Camera _cam;
+    [SerializeField] float _reduceSpeed = 2f;
+    float _target = 1f;
+
+    void Start()
+    {
+        _cam = Camera.main;
+    }
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - _cam.transform.position);
+        _healthBarSprite.fillAmount = Mathf.MoveTowards(_healthBarSprite.fillAmount, _target, _reduceSpeed * Time.deltaTime);
+    }
+    public void UpdateHealthBar(float maxHealth , float currentHealth)
+    {
+        _target = currentHealth / maxHealth;
+    }
+}
--- Assets/_myGame/_scripts/Enemy/Health.cs
+++ Assets/_myGame/_scripts/Enemy/Health.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] float health;
+    [SerializeField] float _maxhealth;
+    float _currenthealth;
+    [SerializeField] EnemyHealthBar  _healthBar;
     Enemy _enemy;
 
     void Awake()
     {
         _enemy = GetComponent<Enemy>();
+        _currenthealth = _maxhealth;
+        _healthBar.UpdateHealthBar(_maxhealth, _currenthealth);
     }
 
 
 
     public void TakeDamage(float attackDamage)
     {
-        health -= attackDamage;
-        if (health > 0) return;
+        _currenthealth -= attackDamage;
+        _healthBar.UpdateHealthBar(_maxhealth, _currenthealth);
+        if (_currenthealth > 0) return;
 
         this.gameObject.SetActive(false);
         EnemyDedector.Instance.EnemyList.Remove(_enemy);
